4727a596218a910d8a85e63d7826c986fed6ca66
Helixan <helixan@proton.me>
2024-04-15 03:29:50 -0400

    Added a custom user details service

--- src/main/java/org/cultro/vault/service/CustomUserDetailsService.java
+++ src/main/java/org/cultro/vault/service/CustomUserDetailsService.java
@@ -1,4 +1,28 @@
 package org.cultro.vault.service;
 
-public class CustomUserDetailsService {
+import org.cultro.vault.model.User;
+import org.cultro.vault.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User Not Found with username: " + username);
+        }
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPasswordHash())
+                .authorities("USER")
+                .build();
+    }
 }
